0eb56264a65f2d5134665894bf6e8c880992d2b6
eldiyar_sydykov <eldiyar_sydykov@mail.ru>
2021-06-03 14:59:26 +0600

    Лицензия

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
 } from "scandit-sdk";
 import ScanditBarcodeScanner from 'scandit-sdk-react'
 
-const LICENSE_KEY = 'AYUgrgehJw+6JurcKQWiRq08XVOgArB9gGDVEexqIWjmTG8k6WK1fQBMNO5oZQ9Uk1GGNtR0wAKfVXi/Llw5sOYr6LSQRue9wh9t7ydXwl8IenKwKHOR0B9Feg0rbZH9G3B8kJNfwg1NHrDfqXFQLvp1/v1Zfrzo1k7+tFZHxbLweqPJS1yI4A9ETfzRVjP+a09cXpNJzDO3bCYApno66/xSYcPFZkEV5UDiTAB/UtQJbiVuRV4xWhVf1fNjZzan1UTrClxSeXYGchpxl33FjkBrV5WYU1dS4AoPekxqmxvqZJDrfXTwgFFLD8/xWkGZ/1nNdJ9fvdSeYAImWVdEnPRAYgJHKhwIeHZgEMZxgfjMRex91UcLZmVptFHJUsKDj2z1vZMvv7G7dVaNGX9NCQlRTqGTRJqPklRUNfoT6UexdCTv4GqKvAdNkxq0WXl2Z3GBrZlq8qUZFhCZU0eFwoFQrcHeYxOJLX0JluFWayNMX3E1qXMd8BBzkNilT0DM+DGI6+8+bwmxQGCNlwLntsvucnHPp++AdYskybzLLMxBgiWZ6+znamJQ7ylXSXLlIvre4eD6s0WTEFN1DGKFeOnIKdEzjpysxVlXol6vrIsgFE3Qxu9G65y/qJPJmk/+61pxyORd1sthscd5iTLKoqIfcHljqYDPPKIDBo0XCLPNa14lMWsch8ZSsp/qUjvy9MdIMA5ULKF+D8siyG03plRAStLi7Ph60o/9kMVS8M1wjPccvFptN6RAU/FtMfWiqeZKGiOFxkv4TD6WnvIVQ8k8a6LyBF2/wh/fXB9gQkr0vTgoAwVL/Er/AQUc3oV2CntgxqNMpVs66I2u7zm1GAGJd9rucgiTO+XeCjligv7ma3ZgNpIoCy6zrUazf5yU5q6FjyldQRmdnaqzzfzezdeFsQJS2M/klMRhkAq5T0/gGKbYlkmigSpASojMG6vETlmPCu2B4ugOND2/nlxp5twz6sb8Dncbwf+SSfg83T7SX3CcSLaQkopalJkt+6eAg5xZrEG81beB8tDLLD/MgTY71mm7/p3fxVa5Aw6ID+y2jyhTRnk1jKzJebPWa5LpfVGR9LvbPt5+aNEQrR+Ib9LzvvGykxcK28cuwK3vb1mmnkkQKKkOaCYBEfCa7wLTBteOmxQ/N7I+fYnYZ3kGipuGGpZYAkia7NVDbOQVn/geMPGCxrxiAs80wDz8/LdJ'
+const LICENSE_KEY = 'ASBQ7CuhC1P5OGku3x5EGb8Mj8O7F2tO00MWleBdXSUcKnXoAXO96lx/yH5JRvzxMHTcBiEYiNmWacKfTn016ahruJmyXPVcTkYsOgJtk5c+HNdy70Qf8oofOkKAQqAgd2H1RbtwLd/GffyDFJccRZLV/znSq/ZCJv81zqmR1eHcMqxJ0Xwk3N/0nquAgMV+G9jByq2iTkaSLUwb9wzgb9t2YZM7REXxhjcy+KFOJpmwYngfmS/qUn+dGDfO/zIAzIv0ZgnJLUGxsk1kzHj9qZCvGFc/OOUOfEfQy4tXRtnqscQdAazVWqFzfPHohEhcizBQMzcWKdszZZcpsT5vuxZFJddjH8uPHW+xJBV6+e1CO7SvCL/PRXaqANy3y8UsB4hOsLBEQnr6iDYM7hMomfPjxdPFM0WgHAm22Qbd7QdKoqpkWlE6l8DEA2lfMtbTlg9iAuRIp4sHqvQVs15JigScWlVSV2yC2vnRUcoD3qs5wyCqQH10lEhOv8nl5Yj4VtPQuH7R3LL/i6MmGGUbtKwsm3H/9MRtOA4rMkD3duOcz/RSilnwzpmPJCniXWknpckj4RdVEZdzqwi67Qq2ZY4jOFXPpIb7OeU4vOoOS/IUVthoJ3/a2WZ9O177f6m+cLEOrwG9BhZp7vtSYiULpSN9bPybIzxLdGXSawq5fo6HB3k6rENvBpU59abDlCzr9Elb2J54nys9CvkXXYymw2mNAd2iEdpc6+o8LWKxhOEFHZYRlIdoLVQ0N3WQ5XLfcdd69niL1RGh5V9G1jNKwX+0B1LFolgM4jHry/g+QDF0'
 
 dotenv.config()
 
@@ -37,6 +37,7 @@ const App = () => {
           ? <div>
             <ScanditBarcodeScanner
               licenseKey={LICENSE_KEY}
+              engineLocation="https://cdn.jsdelivr.net/npm/scandit-sdk@5.6.0/build"
               onScan={setResult}
               onScanError={setResult}
               scanSettings={getScanSettings()}
